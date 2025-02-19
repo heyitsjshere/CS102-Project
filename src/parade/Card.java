8fd68ab0aeadc7f4a0f4e224d8751c4edab1d9c7
cheryl-l <cheryl.lhe04@gmail.com>
2025-02-19 18:15:20 +0800

    create card object

---
+++ src/parade/Card.java
@@ -0,0 +1,24 @@
+package parade;
+import parade.enums.*;
+
+public class Card {
+    private int num;
+    private Colour colour;
+
+    // constructor
+    public Card (int n, Colour c) {
+        this.num = n;
+        this.colour = c;
+    }
+
+    public int getCardNum() {
+        return this.num;
+    }
+
+    public Colour getCardColour() {
+        return this.colour;
+    }
+
+    
+
+}
