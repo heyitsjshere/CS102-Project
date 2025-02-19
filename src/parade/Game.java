edbfe0c4fe44103061d36987d290a754130a5235
cheryl-l <cheryl.lhe04@gmail.com>
2025-02-19 19:33:27 +0800

    added file used to test

---
+++ src/parade/Game.java
@@ -0,0 +1,32 @@
+package parade;
+
+import java.util.ArrayList;
+
+public class Game {
+
+    // assume here game starts, everything tested here for now
+    public static void main (String[] args) {
+        ArrayList<Card> initialHand1 = new ArrayList<>();
+        ArrayList<Card> initialHand2 = new ArrayList<>();
+
+        Deck d = new Deck();
+        for (int i = 0; i < 5; i++) {
+            initialHand1.add(d.drawCard());
+            initialHand2.add(d.drawCard());
+
+        }
+    
+        Player p1 = new Player(initialHand1);
+        Player p2 = new Player(initialHand2);
+
+        System.out.println(p1.getHand());
+        System.out.println(p1.getCollectedCards());
+
+        System.out.println("Size of deck: " + d.getSize()); // 56
+
+
+        // now p1 picks one card
+        
+    }
+    
+}
