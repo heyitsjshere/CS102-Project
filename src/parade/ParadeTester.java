ec424f7bc3f425fe6ba770c9b052ce9982728fbd
cheryl-l <cheryl.lhe04@gmail.com>
2025-02-20 18:46:47 +0800

    tester works for a single human player on console

--- src/parade/ParadeTester.java
+++ src/parade/ParadeTester.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class ParadeTester {
 
-    public static void getUserInput(Player p) {
+    public static Card getUserInput(Player p) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("PICK A CARD");
@@ -18,17 +18,24 @@ public class ParadeTester {
             i++;
         }
 
-        do {
-            System.out.printf("Selection: Option ");
-            if (!sc.hasNextInt()) {
-                System.out.println("That is not a valid option.");
-                System.out.printf("Selection: Option ");
-            }
-        } while (!sc.hasNextInt());
-        
+        // do {
+        //     System.out.printf("Selection: Option ");
+        //     if (!sc.hasNextInt()) {
+        //         System.out.println("That is not a valid option. Please enter an integer");
+        //         System.out.printf("Selection: Option ");
+        //     }
+        // } while (sc.hasNextInt() == false); // need better handling but just assume user enters correct thing for now
+
+        System.out.printf("Selection: Option ");
         int selectedNum = sc.nextInt();
-        Card selectedCard = p.playCard(selectedNum);
-        System.out.println("\nYou have selected Option " + selectedNum + ": " + selectedCard);
+        Card selectedCard = p.getHand().get(selectedNum+1);
+
+        sc.close();
+        
+        System.out.println("-----------");
+        System.out.println("You have selected Option " + selectedNum + ": " + selectedCard);
+
+        return selectedCard;
     }
 
     
@@ -40,30 +47,34 @@ public class ParadeTester {
         for (int i = 0; i < 5; i++) {
             initialHand1.add(d.drawCard());
             initialHand2.add(d.drawCard());
-
         }
-    
         Player p1 = new HumanPlayer(initialHand1);
         Player p2 = new HumanPlayer(initialHand2);
 
-        System.out.println(p1.getHand());
-        System.out.println(p1.getCollectedCards());
+        Parade curParade = new Parade(new ArrayList<Card>(){{ // initialise parade with 6 cards
+            for (int i = 0; i < 6; i++) add(d.drawCard());
+        }});
+
+        System.out.println("Player 1's Hand: " + p1.getHand());
+        System.out.println("Player 1's Collection: " + p1.getCollectedCards());
+        System.out.println("Parade: " + curParade.getParade());
 
         System.out.println("Size of deck: " + d.getSize()); // 56
 
         // now p1 picks one card, card is removed from player's hand
+        Card pickedCard = getUserInput(p1); 
 
+        System.out.println("Removable: " + curParade.getRemoveable(pickedCard));
+        ArrayList<Card> toCollect = curParade.getCollectibleCards(pickedCard);
+        p1.collectCard(toCollect);
+        System.out.println("p1 should collect: " + toCollect);
 
+        curParade.addCard(p1.playCard(pickedCard));
+        p1.addCard(d.drawCard());
+        System.out.println("Player 1's Hand: " + p1.getHand());
+        System.out.println("Player 1's Collection: " + p1.getCollectedCards());
+        System.out.println("Parade: " + curParade.getParade());
 
-        // card is added to the parade
-
-        // get cards in removal mode
-
-        // eligible cards are added to player's collection
-
-        // player draws a card
-
-        // next player's turn
-        
+        System.out.println("Size of deck: " + d.getSize()); // 56
     }
 }
