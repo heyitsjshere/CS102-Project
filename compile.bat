f9aee9d90a56afe00529824a61a86e05ccef6f54
cheryl-l <cheryl.lhe04@gmail.com>
2025-02-20 20:17:56 +0800

    updated compile and run files

--- compile.bat
+++ compile.bat
@@ -1,3 +1,2 @@
-javac -d "classes" -cp "source:mailapi-1.5.5.jar" source/demo/Tester.java 
 
-:: not the real thing, just here to remember the format
+javac -d "classes" -cp "src" src/parade/ParadeTester.java 
--- run.bat
+++ run.bat
@@ -1,3 +1,1 @@
-java -cp "classes:mailapi-1.5.5.jar:activation.jar:smtp-1.5.5.jar" demo.Tester
-
-:: not the real thing, just here to remember the format
+java -cp "classes" parade.ParadeTester;
